d043f60290032241165b5d750582bab969a62f4d
easing <easing@onyx-international.com>
2024-05-14 08:45:12 +0800

    Extract Plugin as a Module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,40 +1,16 @@
-pluginManagement {
-    plugins {
-        id("org.jetbrains.kotlin.jvm") version "1.9.23"
-        id("com.gradle.plugin-publish") version "1.2.1"
-        id("com.github.johnrengelman.shadow") version "8.1.1"
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
-        maven("https://maven.aliyun.com/repository/public/")
-        maven("https://maven.aliyun.com/repository/central/")
-        maven("https://maven.aliyun.com/repository/gradle-plugin/")
-        maven("https://maven.aliyun.com/repository/apache-snapshots/")
         mavenLocal()
         gradlePluginPortal()
-        google()
-        mavenCentral()
     }
 }
 
 buildscript {
     repositories {
-        maven("https://maven.aliyun.com/repository/public/")
-        maven("https://maven.aliyun.com/repository/central/")
-        maven("https://maven.aliyun.com/repository/gradle-plugin/")
-        maven("https://maven.aliyun.com/repository/apache-snapshots/")
-        maven("https://plugins.gradle.org/m2/")
         mavenLocal()
         gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.github.johnrengelman:shadow:8.1.1")
     }
 }
 
-include(":app")
+includeBuild("plugin")
